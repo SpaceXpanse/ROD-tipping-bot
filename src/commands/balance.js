33dd50284a1781e2fdf55f880d88c8e7fc530aa9
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2023-06-08 17:42:07 +0200

    Update balance.js

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,7 +1,7 @@
 const { OOPS_TEXT } = require('../messages')
 const { getBalance } = require('../rodApi')
 
-const BALANCE_TEXT = 'Balance : '
+const BALANCE_TEXT = 'Balance: '
 
 function balance(interaction) {
   const account = interaction.user.id;
@@ -10,15 +10,9 @@ function balance(interaction) {
     .then(function(balance) {
       console.log(balance);
       if (balance === 0) {
-        throw new Error('0');
-      } else {
+        interaction.reply('Your ROD balance is 0.00. Use /rod address to add some.');
+      } else if (balance > 0) {
         interaction.reply(BALANCE_TEXT + balance.toFixed(2) + ' ROD');
-      }
-    })
-    .catch(function(err) {
-      console.log(err);
-      if (err.message === '0') {
-        interaction.reply('Your balance is 0.00');
       } else {
         interaction.reply(OOPS_TEXT);
       }
