934f53ccecaaeb620666c8b4d66c97f3dd339ab8
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2023-06-08 16:17:10 +0200

    Update rodApi.js

--- src/rodApi.js
+++ src/rodApi.js
@@ -20,35 +20,37 @@ function getAccountAddress(account) {
     .then(function(result) {
       const addresses = result.data.result;
 
-      if (addresses !== null && Object(addresses)[0].length > 0) {
-        const firstAddress = Object(addresses)[0];
-        return addresses[firstAddress];
-      } else {
-        return axios
-          .post(ROD_NODE_URL, {
-            jsonrpc: '2.0',
-            id: +new Date(),
-            method: 'getnewaddress',
-            params: [account],
-          }, {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Basic ${BASIC_AUTH_ROD_TOKEN}`,
-            },
-          })
-          .then(function(newAddressResult) {
-            return newAddressResult.data.result;
-          })
-          .catch(function(error) {
-            console.error('Error occurred while getting new address:', error);
-          });
+      if (addresses !== null && Object.keys(addresses).length > 0) {
+        const firstAddress = Object.keys(addresses)[0];
+        const firstAddressObject = addresses[firstAddress];
+        if (Object.keys(firstAddressObject).length > 0) {
+          return firstAddressObject;
+        }
       }
+
+      return axios
+        .post(ROD_NODE_URL, {
+          jsonrpc: '2.0',
+          id: +new Date(),
+          method: 'getnewaddress',
+          params: [account],
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${BASIC_AUTH_ROD_TOKEN}`,
+          },
+        })
+        .then(function(newAddressResult) {
+          return newAddressResult.data.result;
+        })
+        .catch(function(error) {
+          console.error('Error occurred while getting new address:', error);
+        });
     })
     .catch(function(error) {
       console.error('Error occurred while getting account address:', error);
     });
 }
-
 
 function getBalance (account) {
   return axios.post(ROD_NODE_URL, {
