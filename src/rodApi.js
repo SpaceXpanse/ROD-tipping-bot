f4b839862863612da4143e3da4a905d3b353672c
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2023-06-09 14:08:18 +0200

    Update rodApi.js

--- src/rodApi.js
+++ src/rodApi.js
@@ -90,11 +90,11 @@ function move(fromAccount, toAccount, amount) {
         id: +new Date(),
         method: 'send',
         params: [
-          '\'{\"' + toAddress + '\": ' + amount + '}\'',
+          \'{\"' + toAddress + '\": ' + amount + '}\',
           null,
           'unset',
           null,
-          '\'{\"change_address\": \"' + fromAddress + '\"}\''
+          \'{\"change_address\": \"' + fromAddress + '\"}\'
         ]
       }, {
         headers: {
